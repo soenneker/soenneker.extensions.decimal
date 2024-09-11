e311d73b0096e06538d47c463c38b0414bc39bbf
Jake Soenneker <jake@soenneker.com>
2024-09-11 09:10:28 -0500

    Aggressive inlining

--- src/DecimalExtension.cs
+++ src/DecimalExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace Soenneker.Extensions.Decimal;
 
@@ -11,6 +12,7 @@ public static class DecimalExtension
     /// <param name="value"></param>
     /// <param name="excludePlaces">If set to true, will not include 2 decimal places</param>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string? ToCurrencyDisplay(this decimal? value, bool excludePlaces = false)
     {
         return value?.ToCurrencyDisplay(excludePlaces);
@@ -20,6 +22,7 @@ public static class DecimalExtension
     /// <param name="value"></param>
     /// <param name="excludePlaces">If set to true, will not include 2 decimal places</param>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string ToCurrencyDisplay(this decimal value, bool excludePlaces = false)
     {
         string formatter = excludePlaces ? "C0" : "C";
@@ -29,6 +32,7 @@ public static class DecimalExtension
 
     /// <summary>Shorthand for <see cref="Math.Round(decimal, int)"/> (with 2 decimal places) </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static decimal ToCurrency(this decimal value)
     {
         return Math.Round(value, 2);
@@ -37,6 +41,7 @@ public static class DecimalExtension
     /// <summary> Two decimal places, with rounding. i.e. .72948615 -> 72.95% </summary>
     /// <returns>0 will return 0%</returns>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string ToPercentDisplay(this decimal value)
     {
         if (value == 0)
@@ -49,6 +54,7 @@ public static class DecimalExtension
     /// Shorthand for <see cref="decimal.ToDouble"/>
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static double ToDouble(this decimal value)
     {
         return decimal.ToDouble(value);
@@ -58,12 +64,14 @@ public static class DecimalExtension
     /// Shorthand for <see cref="Math.Round(decimal, int)"/>
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static decimal ToRounded(this decimal value, int digits)
     {
         return Math.Round(value, digits);
     }
 
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static decimal? ToRounded(this decimal? value, int digits)
     {
         if (value == null)
@@ -76,6 +84,7 @@ public static class DecimalExtension
     /// Shorthand for <see cref="Convert.ToInt32(decimal)"/>
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static int ToInt(this decimal value)
     {
         return Convert.ToInt32(value);
@@ -85,6 +94,7 @@ public static class DecimalExtension
     /// numerator / denominator (unless denominator = 0, returns 0)
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static decimal SafeDivision(this decimal numerator, decimal denominator)
     {
         if (denominator == 0)
