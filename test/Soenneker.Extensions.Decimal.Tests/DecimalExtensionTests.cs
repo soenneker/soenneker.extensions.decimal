d1d82955d0263bff483c6f62ebc0beb290f1dc00
Jake Soenneker <jake@soeneker.com>
2024-12-20 17:34:19 +0000

    Tests

---
+++ test/Soenneker.Extensions.Decimal.Tests/DecimalExtensionTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.Decimal.Tests;
+
+public class DecimalExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
